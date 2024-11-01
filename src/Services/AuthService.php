464ef11d1a296e69ce81bda7aa98fa9c139a4f0b
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2024-11-01 18:49:25 +0330

    .

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -5,7 +5,7 @@ use Donapp\Models\User;
 
 class AuthService {
     public function checkIfUserLoggedIn() {
-
-        return function_exists('is_user_logged_in');
+        
+        return is_user_logged_in();
     }
 }
