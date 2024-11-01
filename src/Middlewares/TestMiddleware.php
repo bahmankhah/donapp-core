1bcea100b35549da2c338fcb8c4766b85d47cca7
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2024-11-01 21:07:04 +0330

    pipeline

--- src/Middlewares/TestMiddleware.php
+++ src/Middlewares/TestMiddleware.php
@@ -4,6 +4,7 @@ namespace Donapp\Middlewares;
 use Kernel\Middleware;
 class TestMiddleware implements Middleware{
     public function handle($pipeline){
+        die('amir');
         return $pipeline->next();
     }
 }
