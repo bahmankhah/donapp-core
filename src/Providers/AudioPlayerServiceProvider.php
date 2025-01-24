20485bc5480083c1dee98ead63e6c5caf0db6e6f
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2025-01-24 13:22:35 +0330

    added audio player

--- src/Providers/AudioPlayerServiceProvider.php
+++ src/Providers/AudioPlayerServiceProvider.php
@@ -8,14 +8,16 @@ class AudioPlayerServiceProvider
 
     public function boot()
     {
-        wp_register_script(
-            'custom-audioplayer', // Handle
-            WP_PLUGIN_DIR . '/' . appConfig('app.name') . '/'. 'resources/js/audioplayer.js', // Path to the script
-            array('jquery'), // Dependencies (e.g., jQuery)
-            time(), // Version
-            true // Load in footer
-        );
-        wp_enqueue_script('custom-audioplayer');
-        add_action('wp_enqueue_scripts', 'custom-audioplayer');
+        
+        add_action('wp_enqueue_scripts', function(){
+            wp_register_script(
+                'custom-audioplayer', // Handle
+                WP_PLUGIN_DIR . '/' . appConfig('app.name') . '/'. 'resources/js/audioplayer.js', // Path to the script
+                array('jquery'), // Dependencies (e.g., jQuery)
+                time(), // Version
+                true // Load in footer
+            );
+            wp_enqueue_script('custom-audioplayer');
+        });
     }
 }
