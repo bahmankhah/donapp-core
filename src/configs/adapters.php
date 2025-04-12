c2b78ed09e71abb9c78b58a7ed4d5dd02a5f724b
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2025-04-12 11:59:31 +0330

    adapter.php

--- src/configs/adapters.php
+++ src/configs/adapters.php
@@ -7,11 +7,21 @@ return [
             'donap' => [
                 'context' => App\Adapters\Vendor\Contexts\Donap::class,
                 'key' => getenv('DONAPP_EXT_API_KEY'),
-                'access_url'=>'https://api.nraymanstage.donap.ir/external-services/donap-payment-status/',
-                'purchased_redirect_url'=>'https://student.nraymanstage.donap.ir/myProducts/{slug}',
-                'product_page'=>'https://student.nraymanstage.donap.ir/products/details/{slug}',
-                'main_url'=>'https://student.nraymanstage.donap.ir'
+                'access_url' => 'https://api.nraymanstage.donap.ir/external-services/donap-payment-status/',
+                'purchased_redirect_url' => 'https://student.nraymanstage.donap.ir/myProducts/{slug}',
+                'product_page' => 'https://student.nraymanstage.donap.ir/products/details/{slug}',
+                'main_url' => 'https://student.nraymanstage.donap.ir'
             ]
         ]
-    ]
+    ],
+
+    'auth' => [
+        'default' => 'token',
+        'contexts' => [
+            'token' => [
+                'context' => Kernel\Auth\Guards\TokenGuard::class,
+                'provider'=> null,
+            ]
+        ]
+    ],
 ];
