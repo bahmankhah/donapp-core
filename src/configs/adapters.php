90960b96c0d9c3ca34b63679e4b8c8d51fcd55d7
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2025-04-19 11:30:11 +0330

    changed donap urls

--- src/configs/adapters.php
+++ src/configs/adapters.php
@@ -7,10 +7,10 @@ return [
             'donap' => [
                 'context' => App\Adapters\Vendor\Contexts\Donap::class,
                 'key' => getenv('DONAPP_EXT_API_KEY'),
-                'access_url' => 'https://api.nraymanstage.donap.ir/external-services/donap-payment-status/',
-                'purchased_redirect_url' => 'https://student.nraymanstage.donap.ir/myProducts/{slug}',
-                'product_page' => 'https://student.nraymanstage.donap.ir/products/details/{slug}',
-                'main_url' => 'https://student.nraymanstage.donap.ir'
+                'access_url' => 'https://api.rayman.donap.ir/external-services/donap-payment-status/',
+                'purchased_redirect_url' => 'https://rayman.donap.ir/myProducts/{slug}',
+                'product_page' => 'https://rayman.donap.ir/products/details/{slug}',
+                'main_url' => 'https://rayman.donap.ir'
             ]
         ]
     ],
