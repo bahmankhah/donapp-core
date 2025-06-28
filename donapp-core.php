0b05dfca566ef03267020d77729831e053fcbef1
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2025-06-28 23:18:04 +0330

    test

--- donapp-core.php
+++ donapp-core.php
@@ -57,7 +57,7 @@ Wordpress::filter('login_url', function ($login_url, $redirect, $force_reauth) {
     appLogger('setting login url2');
     $url = "https://authplatform.donap.ir/realms/donap/protocol/openid-connect/auth?client_id=market&response_type=code";
     return $url;
-}, 5, 3);
+}, 1, 3);
 // function custom_footer_script()
 // {
 //     // Register the script
