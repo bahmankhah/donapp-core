5d5853befb86be01839ab123a5391b3689fc993c
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2025-06-28 22:17:16 +0330

    test

--- src/Providers/HookFilterServiceProvider.php
+++ src/Providers/HookFilterServiceProvider.php
@@ -42,7 +42,7 @@ class HookFilterServiceProvider
         add_filter('login_url', function () {
             appLogger('setting login url');
             return Auth::sso()->getLoginUrl();
-        }, 10, 2);
+        }, 5, 1);
         
         add_filter('woocommerce_get_item_data', function ($item_data, $cart_item) {
             if (!empty($cart_item['wallet_topup'])) {
