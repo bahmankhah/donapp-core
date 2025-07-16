be5405ef61383b676e2df9df45b76c9973764ef0
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2025-07-16 21:53:27 +0330

    fixed elemento dynamic tags

--- src/Providers/ElementorServiceProvider.php
+++ src/Providers/ElementorServiceProvider.php
@@ -11,7 +11,14 @@ class ElementorServiceProvider
 
     public function boot()
     {
+        // Register custom group first
         add_action('elementor/dynamic_tags/register_tags', function ($dynamic_tags) {
+            // Register the custom group
+            $dynamic_tags->register_group('donap', [
+                'title' => 'دناپ' // Persian title for your group
+            ]);
+            
+            // Then register the tags
             $dynamic_tags->register_tag(WalletCreditValue::class);
             $dynamic_tags->register_tag(WalletCashValue::class);
         });
