fe3e96fbc38d7cd7af6e9457d82db96802eb1fd7
Hesam <bahmankhah1@gmail.com>
2025-09-14 22:25:09 +0330

    test

--- src/Services/GravityService.php
+++ src/Services/GravityService.php
@@ -960,7 +960,7 @@ class GravityService
             $current_user = $user ?? wp_get_current_user();
             appLogger(json_encode($current_user));
             $user_id = $current_user->ID ?? get_current_user_id();
-            die($user_id);
+            wp_die($user_id);
             if (!$user_id) {
                 return [
                     'success' => false,
