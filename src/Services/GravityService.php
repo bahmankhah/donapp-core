d77800555f2c1a2ad869f1f6d28a7680a1407f4e
Hesam <bahmankhah1@gmail.com>
2025-11-28 22:20:17 +0330

    testing pdf exporter in approved forms

--- src/Services/GravityService.php
+++ src/Services/GravityService.php
@@ -69,7 +69,7 @@ class GravityService
             // Check if this form has Gravity Flow enabled
             $form_id = $form['id'];
 
-            if(isset($filters['form_filter'])){
+            if(isset($filters['form_filter']) && !empty($filters['form_filter'])){
                 if($filters['form_filter'] != $form_id){
                     // appLogger('GravityService: Skipping form ID ' . $form_id . ' due to filter');
                     continue;
